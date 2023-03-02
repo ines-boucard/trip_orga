9d9cb4fa33b3fcab6f3475c4fa5eeb8add654176
ines-boucard <72189183+ines-boucard@users.noreply.github.com>
2023-03-02 15:54:04 +0100

    first commit

---
+++ src/main/java/com/example/trip_orga/TripOrgaApplication.java
@@ -0,0 +1,13 @@
+package com.example.trip_orga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripOrgaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripOrgaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trip_orga/TripOrgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trip_orga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripOrgaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
